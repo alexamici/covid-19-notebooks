97b699b77911f9f3c3b65e0ec6d814bc6f3086c9
Alessandro Amici <a.amici@bopen.eu>
2020-03-07 16:59:30 +0100

    Situation report plot almost ready

--- covid19/plot.py
+++ covid19/plot.py
@@ -16,16 +16,18 @@ def plot(data, fit, label=None, extrapolate=(None, None)):
     y_predict = fit.predict(x_predict)
     plt.plot(x_predict, y_predict, ":")
 
-    x_fit = data[fit.start:fit.stop].index.values
+    x_fit = data[fit.start : fit.stop].index.values
     y_fit = fit.predict(x_fit)
-    label_fit = f"estimated {label} $T_d={fit.T_d_days:.2f}$ days, $t_0=${str(fit.t_0)[:10]}"
+    label_fit = (
+        f"estimated {label} $T_d={fit.T_d_days:.2f}$ days, $t_0=${str(fit.t_0)[:10]}"
+    )
     plt.plot(x_fit, y_fit, ".-", label=label_fit)
 
-    ax = sns.scatterplot(data=data[fit.start:fit.stop], label=label, s=70)
+    ax = sns.scatterplot(data=data[fit.start : fit.stop], label=label, s=70)
     if fit.start is not None:
-        sns.scatterplot(data=data[data.index < fit.start], marker='x', s=70)
+        sns.scatterplot(data=data[data.index < fit.start], marker="x", s=70)
     if fit.stop is not None:
-        sns.scatterplot(data=data[data.index > fit.stop], marker='x', s=70)
+        sns.scatterplot(data=data[data.index > fit.stop], marker="x", s=70)
 
     ax.set(xlim=(extrapolate_start, extrapolate_stop))
 
