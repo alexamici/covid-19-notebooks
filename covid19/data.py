af2a833b20ba3a996672cd62749da32a78801c9a
Alessandro Amici <a.amici@bopen.eu>
2020-03-07 15:07:54 +0100

    Refactor the exponential fit to use 2 ** (t - t_0) / dt

--- covid19/data.py
+++ covid19/data.py
@@ -1,13 +1,12 @@
-
 import numpy as np
 
 
-REFERENCE_DATETIME = '2020-02-18T16:00'
+REFERENCE_DATETIME = "2020-02-18T16:00"
 
 
 def to_days(date, reference_datetime=REFERENCE_DATETIME, hour_of_day=None):
     if isinstance(date, str):
         date = np.datetime64(date)
     if hour_of_day:
-        date = date + np.timedelta64(hour_of_day, 'h')
-    return (date - np.datetime64(reference_datetime)) / np.timedelta64(1, 'D')
+        date = date + np.timedelta64(hour_of_day, "h")
+    return (date - np.datetime64(reference_datetime)) / np.timedelta64(1, "D")
--- covid19/fit.py
+++ covid19/fit.py
@@ -5,46 +5,49 @@ import numpy as np
 import scipy.optimize
 
 
-def exponential(x, a, b):
-    return a * b ** x
+def exp2(t, t_0, dt):
+    return 2 ** ((t - t_0) / dt)
 
 
-def linear(x, y0, m):
-    return m * x + y0
+def linear(t, t_0, dt):
+    return (t - t_0) / dt
 
 
 @attr.attrs()
 class ExponentialFit:
-    r"""$y = a \cdot b ^ \frac{x - x_0}{\Delta x}$"""
-    x_0 = attr.attrib()
-    delta_x = attr.attrib()
+    r"""$y = 2 ^ \frac{t - t_0}{\delta t}$"""
+    t_0 = attr.attrib()
+    dt = attr.attrib()
     start_fit = attr.attrib()
     stop_fit = attr.attrib()
-    a = attr.attrib()
-    b = attr.attrib()
 
     @classmethod
-    def from_frame(cls, x, y, data, start_fit=None, stop_fit=None, x_0=np.datetime64('2020-02-20'), delta_x=np.timedelta64(1, 'D'), p0=(-0.1, 1.)):
-        # x_ 0 defaults to index the Situation Reports from WHO, SR1 was on 2020-01-21
+    def from_frame(
+        cls,
+        y,
+        data,
+        start_fit=None,
+        stop_fit=None,
+        p0=(np.datetime64("2020-02-12T00:00:00"), np.timedelta64(48 * 60 * 60, "s")),
+    ):
+        t_0_guess, dt_guess = p0
 
         data_fit = data[start_fit:stop_fit]
-        x_fit = (data_fit[x].values - x_0) / delta_x
-        y_fit = data_fit[y].values
 
-        (start_fit, stop_fit), (a, b), _ = log_fit(x_fit, y_fit, p0=p0)
+        x_norm = linear(data_fit.index.values, t_0_guess, dt_guess)
+        log2_y = np.log2(data_fit[y].values)
 
-        return cls(x_0, delta_x, start_fit, stop_fit, a, b)
+        x_fit = x_norm[np.isfinite(log2_y)]
+        log2_y_fit = log2_y[np.isfinite(log2_y)]
 
-    def predict(self, x):
-        x_fit = (x - self.x_0) / self.delta_x
-        return exponential(x_fit, self.a, self.b)
+        (t_0_norm, dt_norm), _ = scipy.optimize.curve_fit(
+            linear, x_fit, log2_y_fit, p0=(1.0, 1.0)
+        )
 
+        dt = dt_norm * dt_guess
+        t_0 = t_0_guess + t_0_norm * dt_guess
 
-def log_fit(x, y, func=linear, p0=(-0.1, 1.)):
-    lny = np.log(y)
-    x_fit = x[np.isfinite(lny)]
-    lny_fit = lny[np.isfinite(lny)]
+        return cls(t_0, dt, start_fit, stop_fit)
 
-    params, log_cov = scipy.optimize.curve_fit(func, x_fit, lny_fit, p0=p0)
-
-    return [x_fit[0], x_fit[-1]], [math.exp(p) for p in params], log_cov
+    def predict(self, t):
+        return 2 ** linear(t, self.t_0, self.dt)
