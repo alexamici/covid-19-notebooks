aed57f5ae0cf2faac03cfa6aae958c5440ef2eec
Alessandro Amici <a.amici@bopen.eu>
2020-03-12 12:10:05 +0100

    Add support for more repos and add world data

--- covid19/data.py
+++ covid19/data.py
@@ -1,29 +1,31 @@
 import pathlib
 
-import numpy as np
 import requests
 
-DPC_DATA_REPO = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master'
-DPC_DATA_STREAMS = {
-    'regioni': f'{DPC_DATA_REPO}/dati-regioni/dpc-covid19-ita-regioni.csv',
-    'andamento-nazionale': f'{DPC_DATA_REPO}/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+DATA_REPOS = {
+    'world': {
+        'url': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master',
+        'streams': {
+            'deaths': '{url}/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+        },
+    },
+    'italy': {
+        'url': 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master',
+        'streams': {
+            'regioni': '{url}/dati-regioni/dpc-covid19-ita-regioni.csv',
+            'andamento-nazionale': '{url}/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+        }
+    },
 }
-REFERENCE_DATETIME = "2020-02-18T16:00"
 
 
-def to_days(date, reference_datetime=REFERENCE_DATETIME, hour_of_day=None):
-    if isinstance(date, str):
-        date = np.datetime64(date)
-    if hour_of_day:
-        date = date + np.timedelta64(hour_of_day, "h")
-    return (date - np.datetime64(reference_datetime)) / np.timedelta64(1, "D")
-
-
-def download(url, path='.'):
-    url = DPC_DATA_STREAMS.get(url, url)
+def download(url, path='.', repo='italy'):
+    repo = DATA_REPOS[repo]
+    base_url = repo['url']
+    stream_url = repo['streams'].get(url, url).format(url=base_url)
     root_path = pathlib.Path(path)
-    download_path = root_path / url.rpartition('/')[2]
-    with requests.get(url) as resp:
+    download_path = root_path / stream_url.rpartition('/')[2]
+    with requests.get(stream_url) as resp:
         with open(download_path, 'wb') as fp:
             fp.write(resp.content)
     return str(download_path)
