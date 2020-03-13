6cbc312a7a218f819ff45f0881ae3b2f63944983
Alessandro Amici <a.amici@bopen.eu>
2020-03-13 23:08:57 +0100

    Switch to use linregress for fitting and store r_squared

--- covid19/fit.py
+++ covid19/fit.py
@@ -20,6 +20,7 @@ class ExponentialFit:
     r"""$f(t) = 2 ^ \frac{t - t_0}{T_d}$"""
     t_0 = attr.attrib()
     T_d = attr.attrib()
+    r_squared = attr.attrib()
     start = attr.attrib()
     stop = attr.attrib()
 
@@ -36,12 +37,15 @@ class ExponentialFit:
         x_fit = x_norm[np.isfinite(log2_y)]
         log2_y_fit = log2_y[np.isfinite(log2_y)]
 
-        (t_0_norm, T_d_norm), _ = scipy.optimize.curve_fit(linear, x_fit, log2_y_fit)
+        # (t_0_norm, T_d_norm), covariance = scipy.optimize.curve_fit(linear, x_fit, log2_y_fit)
+        m, y, r_squared, _, _ = scipy.stats.linregress(x_fit, log2_y_fit)
+        t_0_norm = -y / m
+        T_d_norm = 1 / m
 
         T_d = T_d_norm * T_d_guess
         t_0 = t_0_guess + t_0_norm * T_d_guess
 
-        return cls(t_0, T_d, start=t_fit[0], stop=t_fit[-1])
+        return cls(t_0, T_d, r_squared=r_squared, start=t_fit[0], stop=t_fit[-1])
 
     @property
     def T_d_days(self):
