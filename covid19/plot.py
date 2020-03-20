1b949ce98f73e8eea5024c0814a97670f431d2b0
Alessandro Amici <a.amici@bopen.eu>
2020-03-20 08:57:25 +0100

    Update 202-03-19

--- covid19/plot.py
+++ covid19/plot.py
@@ -61,15 +61,15 @@ def plot(ax, data, fit, label=None, extrapolate=(None, None), color=None, **kwar
 
 ITALY_EVENTS = [
     # {'x': '2020-02-19', 'label': 'First alarm'},
-    {'x': '2020-02-24', 'label': 'North closes schools'},
-    {'x': '2020-03-01', 'label': 'North partial lockdown'},
-    {'x': '2020-03-05', 'label': 'Italy closes schools'},
-    {'x': '2020-03-08', 'label': 'North lockdown'},
-    {'x': '2020-03-10', 'label': 'Italy lockdown'},
+    {'x': '2020-02-24', 'label': 'Chiusura scuole al nord'},
+    {'x': '2020-03-01', 'label': 'Lockdown parziale al nord'},
+    {'x': '2020-03-05', 'label': 'Chiusura scuole in Italia'},
+    {'x': '2020-03-08', 'label': 'Lockdown al nord'},
+    {'x': '2020-03-10', 'label': 'Lockdown in Italia'},
 ]
 
 
 def add_events(ax, events=ITALY_EVENTS, offset=0, **kwargs):
     for event in events:
-        label = '{x} {label}'.format(**event)
+        label = '{x} + {offset} {label}'.format(offset=offset, **event)
         ax.axvline(x=np.datetime64(event['x']) + np.timedelta64(offset * 24 * 60 * 60, 's'), label=label, **kwargs)
